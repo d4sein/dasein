5cb918672516bf8f145170949327d822d8ab6f2b
Victor Freire <freirel.victor@gmail.com>
2021-01-23 21:00:53 -0300

    add prefix to router

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -6,7 +6,6 @@ import (
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/d4sein/Dasein/config"
 )
 
 const (
@@ -17,6 +16,7 @@ const (
 // Router defines the structure of the Router
 type Router struct {
 	Commands map[string]Command
+	prefix   string
 }
 
 // Command defines the structure of a command
@@ -31,6 +31,7 @@ type callback func(s *discordgo.Session, m *discordgo.MessageCreate)
 func New() Router {
 	return Router{
 		Commands: make(map[string]Command),
+		prefix:   "!",
 	}
 }
 
@@ -45,7 +46,7 @@ func (r Router) AddCommand(c Command) {
 func (r Router) parseCommand(s *discordgo.Session, m *discordgo.MessageCreate) (Command, error) {
 	ctx := strings.Split(m.Content, " ")
 
-	name := strings.TrimPrefix(ctx[0], config.Config.Prefix)
+	name := strings.TrimPrefix(ctx[0], r.prefix)
 
 	if cmd, ok := r.Commands[name]; ok {
 		return cmd, nil
@@ -71,4 +72,9 @@ func (r Router) OnMessageCreateHandler(s *discordgo.Session, m *discordgo.Messag
 	}
 
 	cmd.Run(s, m)
+}
+
+func (r Router) SetPrefix(prefix string) Router {
+	r.prefix = prefix
+	return r
 }
